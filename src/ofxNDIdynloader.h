98a8f40948f5cda91f0022a0748464930e9b9ee2
zilog <dropmeaword@gmail.com>
2019-12-05 20:21:09 +0100

    rewrote the dynloader using BOOST in a platform independant way

--- src/ofxNDIdynloader.h
+++ src/ofxNDIdynloader.h
@@ -4,23 +4,31 @@
 #include <string>
 #include <vector>
 
+#include <boost/dll/import.hpp>         // for dll::import
+#include <boost/dll/shared_library.hpp> // for dll::shared_library
+#include <boost/function.hpp>
+
+namespace dll = boost::dll;
+
 #include "Processing.NDI.Lib.h" // NDI SDK
 
-typedef NDIlib_v4* (*NDIlib_v4_load_)(void);
+typedef NDIlib_v4* (NDIlib_v4_load_)(void);
 
 
 class ofxNDIdynloader
 {
-	const NDIlib_v4* p_NDILib;
-    bool             m_bWasLoaded;
-    
+    const NDIlib_v4* p_NDILib;
+
+    dll::shared_library dynlib;
     const std::string FindRuntime();
 
 public:
     ofxNDIdynloader();
+    ~ofxNDIdynloader();
 
     /** load library dynamically */
     const NDIlib_v4* Load();
+    const NDIlib_v4* Ref() { return p_NDILib; }
 };
 
 #endif // OFXNDIDYNLOADER_H
